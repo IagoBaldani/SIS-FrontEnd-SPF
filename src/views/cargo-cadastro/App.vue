aae3c089b2932b442f98f558612631454b4c77c9
Marco Aguiar <marcoaguiar@sisconsultoria.com.br>
2021-12-13 09:23:45 -0300

    Correção de criação de cargo

--- src/views/cargo-cadastro/App.vue
+++ src/views/cargo-cadastro/App.vue
@@ -12,50 +12,58 @@
                     <div>
                         <label for="cargoInput" class="form-label mb-0 titulo">Cargo</label>
                         <input class="form-control" v-bind:value="cargos.cargo" type="text" name="cargo" id="inputCargo"/>
-                        <p id="erroCargo" class="erro none">Por favor, selecione um cargo </p>
+                        <p id="erroCargo" class="erro none">Por favor, selecione um cargo</p>
                         <p id="erroCargoCadastrado" class="erro none">Este cargo já está cadastrada, por favor insira outro</p>
                     </div>
                     <div>
                         <label for="bolsaAuxilio" class="form-label mb-0 mt-3 titulo">Bolsa auxílio</label>
                         <input type="number" name="bolsaAuxilio" class="form-control" v-bind:value="cargos.bolsa" id="inputBolsaAuxilio"/>
-                        <p id="erroBolsa" class="erro none">Por favor, coloque um valor válido maior ou igual a 0. </p>
+                        <p id="erroBolsa" class="erro none">Por favor, preencha este campo</p>
+                        <p id="erroBolsaQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                     <div>
                         <label for="beneficios" class="form-label mb-0 mt-3 titulo">Benefícios</label>
                         <input type="number" name="beneficios" class="form-control" v-bind:value="cargos.beneficio" id="inputBeneficios"/>
-                        <p id="erroBeneficios" class="erro none">Por favor, coloque um valor válido maior ou igual a 0. </p>
+                        <p id="erroBeneficios" class="erro none">Por favor, preencha este campo</p>
+                        <p id="erroBeneficiosQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                     <div>
                         <label for="convenio" class="form-label mb-0 mt-3 titulo">Convênio</label>
                         <input type="number" class="form-control" name="convenio" v-bind:value="cargos.convenio" id="inputConvenio"/>
-                        <p id="erroConvenio" class="erro none">Por favor, coloque um valor válido maior ou igual a 0.</p>
+                        <p id="erroConvenio" class="erro none">Por favor, preencha este campo</p>
+                        <p id="erroConvenioQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                     <div class="mb-5">
                         <label for="horaExtra" class="form-label mb-0 mt-3 titulo">Hora extra</label>
                         <input type="number" class="form-control" name="horaExtra" v-bind:value="cargos.horaExtra" id="inputHoraExtra" />
-                        <p id="erroHoraExtra" class="erro none">Por favor, coloque um valor válido maior ou igual a 0.</p>
+                        <p id="erroHoraExtra" class="erro none">Por favor, preencha este campo</p>
+                        <p id="erroHoraExtraQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                 </div>
                 <div class="col-xl-4">
                     <div>
                         <label for="beneficioLegislacao" class="form-label mb-0 titulo">Benefício legislação</label>
                         <input type="number" class="form-control" name="beneficioLegislacao" v-bind:value="cargos.beneficioLegislacao" id="inputBeneficioLegislacao" />
-                        <p id="erroBeneficioLegislacao" class="erro none">Por favor, coloque um valor válido maior ou igual a 0.</p>
+                        <p id="erroBeneficioLegislacao" class="erro none">Por favor, preencha este campo</p>
+                        <p id="erroBeneficioLegislacaoQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                     <div>
                         <label for="remuneracaoEsporadica" class="form-label mb-0 mt-3 titulo">Remuneração esporádica</label>
                         <input name="remuneracaoEsporadica" type="number" class="form-control" v-bind:value="cargos.remunEsporadica" id="inputRemuneracaoEsporadica" />
-                        <p id="erroRemuneracaoEsporadica" class="erro none">Por favor, coloque um valor válido maior ou igual a 0. </p>
+                        <p id="erroRemuneracaoEsporadica" class="erro none">Por favor, preencha este campo </p>
+                        <p id="erroRemuneracaoEsporadicaQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                     <div>
                         <label for="remuneracaoExtra" class="form-label mb-0 mt-3 titulo">Remuneração extra</label>
                         <input name="remuneracaoExtra" type="number" class="form-control" v-bind:value="cargos.remunExtra" id="inputRemuneracaoExtra">
-                        <p id="erroRemuneracaoExtra" class="erro none">Por favor, coloque um valor válido maior ou igual a 0.</p>
+                        <p id="erroRemuneracaoExtra" class="erro none">Por favor, preencha este campo</p>
+                        <p id="erroRemuneracaoExtraQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                     <div>
                         <label class="form-label mb-0 mt-3 titulo">Alura</label>
                         <input name="alura" type="number" class="form-control" v-bind:value="cargos.alura" id="inputAlura">
-                        <p id="erroAlura" class="erro none">Por favor, coloque um valor válido maior ou igual a 0.</p>
+                        <p id="erroAlura" class="erro none">Por favor, preencha este campo</p>
+                        <p id="erroAluraQuantidade" class="erro none">Por favor, coloque um valor maior ou igual a 0</p>
                     </div>
                 </div>
                 <div class="col-xl-2"></div>
@@ -63,8 +71,9 @@
             <div class="row justify-content-evenly">
                 <div class="col-xl-4">
                     <div>
-                        <button type="button" class="btn-confirmar btn"  @click.prevent="registrarDados">CONFIRMAR</button>
+                        <button type="button" class="btn-confirmar btn"  v-on:click="validaCampos()">CONFIRMAR</button>
                         <p data-bs-toggle="modal" data-bs-target="#exampleModal" class="none" id="abreModal"></p>
+                        <p id="formDados" @click="registrarDados()"></p>
                     </div>
                 </div>
                 <div class="col-xl-4"></div>
@@ -119,7 +128,7 @@
         </div>
         <div class="modal-footer border-0 justify-content-around">
           <div>
-            <button type="button" class="btn" @click="processarDados">CONFIRMAR</button>
+            <button id="botaoModal" type="button" class="btn" @click="processarDados">CONFIRMAR</button>
           </div>
           <div>
             <button type="button" class="btn cancel" data-bs-dismiss="modal">CANCELAR</button>
@@ -220,16 +229,13 @@ export default {
       this.cargoForm.remunEsporadica = document.querySelector('#inputRemuneracaoEsporadica').value
       this.cargoForm.remunExtra = document.querySelector('#inputRemuneracaoExtra').value
       this.cargoForm.alura = document.querySelector('#inputAlura').value
-      if (this.validaCampos()) {
-        document.querySelector('#abreModal').click()
-      }
     },
-    abrirModal() {
-        document.getElementById('abreModalInvisivel').click()
-      },
-    abrirModalCadastro() {
-        document.getElementById('abreModalInvisivelCadastro').click()
-      },
+    abrirModal () {
+      document.getElementById('abreModalInvisivel').click()
+    },
+    abrirModalCadastro () {
+      document.getElementById('abreModalInvisivelCadastro').click()
+    },
     pegaDadosUrl () {
       var query = location.search.slice(1)
       var partes = query.split('&')
@@ -265,8 +271,8 @@ export default {
           .then(response => {
             this.abrirModalCadastro()
             setTimeout(function () {
-            window.location.href = variavel.href = 'cargo-listar'
-          },1521)
+              window.location.href = variavel.href = 'cargo-listar'
+            }, 1521)
           })
           .catch(error => {
             console.log(error)
@@ -287,9 +293,16 @@ export default {
     },
     validaCampos () {
       let cargoDigitado = document.querySelector('#inputCargo').value
+      let bolsaAux = document.querySelector('#inputBolsaAuxilio').value
+      let beneficios = document.querySelector('#inputBeneficios').value
+      let convenio = document.querySelector('#inputConvenio').value
+      let horaExtra = document.querySelector('#inputHoraExtra').value
+      let beneficioLegislacao = document.querySelector('#inputBeneficioLegislacao').value
+      let remuneracaoEsporadica = document.querySelector('#inputRemuneracaoEsporadica').value
+      let remuneracaoExtra = document.querySelector('#inputRemuneracaoExtra').value
+      let alura = document.querySelector('#inputAlura').value
       let erro = 0
-      let vazio = ''
-      if (this.cargoForm.cargo == vazio) {
+      if (cargoDigitado == '') {
         document.querySelector('#erroCargo').classList.remove('none')
         erro = 1
       } else {
@@ -305,58 +318,108 @@ export default {
           }
         }
       })
-      if (this.cargoForm.bolsa == vazio || this.cargoForm.bolsa < 0) {
+      if (bolsaAux == '') {
         document.querySelector('#erroBolsa').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroBolsa').classList.add('none')
       }
-      if (this.cargoForm.beneficio == vazio || this.cargoForm.beneficio < 0) {
+      if (bolsaAux < 0) {
+        document.querySelector('#erroBolsaQuantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroBolsaQuantidade').classList.add('none')
+      }
+      if (beneficios == '') {
         document.querySelector('#erroBeneficios').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroBeneficios').classList.add('none')
       }
-      if (this.cargoForm.convenio == vazio || this.cargoForm.convenio < 0) {
+      if (beneficios < 0) {
+        document.querySelector('#erroBeneficiosQuantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroBeneficiosQuantidade').classList.add('none')
+      }
+      if (convenio == '') {
         document.querySelector('#erroConvenio').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroConvenio').classList.add('none')
       }
-      if (this.cargoForm.horaExtra == vazio || this.cargoForm.horaExtra < 0) {
+      if (convenio < 0) {
+        document.querySelector('#erroConvenioQuantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroConvenioQuantidade').classList.add('none')
+      }
+      if (horaExtra == '') {
         document.querySelector('#erroHoraExtra').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroHoraExtra').classList.add('none')
       }
-      if (this.cargoForm.beneficioLegislacao == vazio || this.cargoForm.beneficioLegislacao < 0) {
+      if (horaExtra < 0) {
+        document.querySelector('#erroHoraExtraQuantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroHoraExtraQuantidade').classList.add('none')
+      }
+      if (beneficioLegislacao == '') {
         document.querySelector('#erroBeneficioLegislacao').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroBeneficioLegislacao').classList.add('none')
       }
-      if (this.cargoForm.remunEsporadica == vazio || this.cargoForm.remunEsporadica < 0) {
+      if (beneficioLegislacao < 0) {
+        document.querySelector('#erroBeneficioLegislacaoQuantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroBeneficioLegislacaoQuantidade').classList.add('none')
+      }
+      if (remuneracaoEsporadica == '') {
         document.querySelector('#erroRemuneracaoEsporadica').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroRemuneracaoEsporadica').classList.add('none')
       }
-      if (this.cargoForm.remunExtra == vazio || this.cargoForm.remunExtra < 0) {
+      if (remuneracaoEsporadica < 0) {
+        document.querySelector('#erroRemuneracaoEsporadicaQuantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroRemuneracaoEsporadicaQuantidade').classList.add('none')
+      }
+      if (remuneracaoExtra == '') {
         document.querySelector('#erroRemuneracaoExtra').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroRemuneracaoExtra').classList.add('none')
       }
-      if (this.cargoForm.alura == vazio || this.cargoForm.alura < 0) {
+      if (remuneracaoExtra < 0) {
+        document.querySelector('#erroRemuneracaoExtraQuantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroRemuneracaoExtraQuantidade').classList.add('none')
+      }
+      if (alura == '') {
         document.querySelector('#erroAlura').classList.remove('none')
         erro = 1
       } else {
         document.querySelector('#erroAlura').classList.add('none')
       }
+      if (alura < 0) {
+        document.querySelector('#erroAluraQUantidade').classList.remove('none')
+        erro = 1
+      } else {
+        document.querySelector('#erroAluraQUantidade').classList.add('none')
+      }
       if (erro == 1) {
         return false
       }
       document.querySelector('#erroCargoCadastrado').classList.add('none')
+      document.querySelector('#formDados').click()
+      document.querySelector('#abreModal').click()
       return true
     }
   }
@@ -441,6 +504,16 @@ body{
   justify-content: flex-start;
   align-items: center;
   padding: 80px;
+}
+
+#botaoModal {
+  background-color: #ab0045;
+  color: white !important;
+  font-weight: bold !important;
+  border-radius: 5px !important;
+  width: 350px;
+  height: 50px;
+  font-size: 25px !important;
 }
 
 .alinharDiv {
