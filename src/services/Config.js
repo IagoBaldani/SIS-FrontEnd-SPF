8b983cfccf5eff940ddc0d64e84292297b6fc604
Leticiasis <90476977+Leticiasis@users.noreply.github.com>
2021-12-23 09:33:32 -0300

    mudança localhost

--- src/services/Config.js
+++ src/services/Config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookie from 'js-cookie'
 
 export const http = axios.create({
-  baseURL: 'http://192.168.30.162:8081/api/',
+  baseURL: 'http://localhost:8081/api/',
   headers: {
     Authorization: `Bearer ${Cookie.get('login_token')}`
   }
