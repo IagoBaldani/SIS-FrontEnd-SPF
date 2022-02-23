acf164ccf34ce12d1c59915351b217a1bba0e435
felipesalmazo82 <felipesalmazo@sisconsultoria.com.br>
2022-02-23 11:52:44 -0300

    commit para deploy em producao

--- src/services/Config.js
+++ src/services/Config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookie from 'js-cookie'
 
 export const http = axios.create({
-  baseURL: 'http://localhost:8081/api/',
+  baseURL: 'http://10.104.1.5:8081/api/',
   headers: {
     Authorization: `Bearer ${Cookie.get('login_token')}`
   }
--- src/services/Funcoes.js
+++ src/services/Funcoes.js
@@ -5,14 +5,14 @@ export default {
   async verificaToken () {
     const token = Cookie.get('login_token')
 
-    await axios.get(`http://localhost:8081/api/auth/${token}`)
+    await axios.get(`http://10.104.1.5:8081/api/auth/${token}`)
       .then(response => {
         this.responseStatus = response.data
         console.log('AUTENTICADO')
       })
 
     if (this.responseStatus == 'ERRO') {
-      window.location.href = 'http://localhost:8080/login'
+      window.location.href = 'http://10.104.1.5:8080/login'
       console.log('NÃO AUTENTICADO')
     }
   }
